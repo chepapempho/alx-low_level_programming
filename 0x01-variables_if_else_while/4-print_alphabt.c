d86aa6fedf7e52e92983f25fc0c72b771666f329
chepapempho <chepapempho@gmail.com>
2022-04-09 16:49:41 +0000

    task 1-5

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,21 +11,15 @@
 
 int main(void)
 {
-	char alphabet;
-	
-	for (alphabet = 'a'; alphabet < 'z'; alphabet++)
-	{
-		if (alphabet == 'q')
-		continue;
-		
-		else if (alphabet == 'e')
-		continue;
-		
-		putchar(alphabet);
-	}
-	
-	
-	putchar ('\n');
-	
-	return (0);
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+if (alphabet == 'q')
+continue;
+else if (alphabet == 'e')
+continue;
+putchar (alphabet);
+}
+putchar ('\n');
+return (0);
 }
