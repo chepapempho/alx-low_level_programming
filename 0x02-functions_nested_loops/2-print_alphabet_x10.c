58e488c5a725211b21c0d6299b41f2eefaea99a4
chepapempho <chepapempho@gmail.com>
2022-04-12 17:03:58 +0000

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/*
+*print_alphabet_x10 - - print lowercase a to z 10 times
+*main - task 2
+*Return: void function returns nothing
+*/
+void print_alphabet_x10(void);
+{
+char alphabet;
+for (int i = 0; i <= 10; i++)
+{
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar (alphabet);
+}
+}
+_putchar ('\n');
+}
