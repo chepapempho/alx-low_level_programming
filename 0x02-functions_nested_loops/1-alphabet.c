b4367fb66b0e13451ca930aa0da97424f19b19df
chepapempho <chepapempho@gmail.com>
2022-04-12 15:16:47 +0000

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,14 @@
 #include "main.h"
-/* 
- * *main - task 1
- * * Return: void functio. returns nothing
- * */
+/**
+ * main - task 1
+ * Return: void functio. returns nothing
+*/
 void print_alphabet(void)
 {
 char alphabet;
 for(alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
-_putchar(alphabet);
+_putchar (alphabet);
 }
-_putchar('\n');
+_putchar ('\n');
 }
