9a0de79be8c1935e991689a03eea1e50673806eb
chepapempho <chepapempho@gmail.com>
2022-04-13 17:04:21 +0000

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- *  * main - main
- *   * Return: 0
- *    */
+ * main - main
+ * Return: 0
+ */
 
 int main(void)
 {
@@ -11,5 +11,6 @@ i = 0;
 
 positive_or_negative(i);
 
-return(0);
+return (0);
+
 }
