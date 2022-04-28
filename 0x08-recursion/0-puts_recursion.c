ca27fc112b607294ab426b719624a50f4f6170fa
chepapempho <chepapempho@gmail.com>
2022-04-28 11:34:27 +0000

    task0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+include "main.h"
+
+/**
+*_puts_recursion - prints a string followed by new line
+*
+*return : nothing
+*/
+
+void _put_recursion(char *s)
+{
+	if (*s == '\0')
+		{
+		_putchar ('\n');
+		return;
+		}
+
+	_putchar (*s);
+	_puts_recursion(s + 1);
+
+}
+
