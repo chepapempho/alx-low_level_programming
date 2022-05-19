ed1558bf58396873c37ad369356482ded0ef3135
chepapempho <chepapempho@gmail.com>
2022-05-19 14:28:13 +0000

    Task one

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,2 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
 
+/**
+  * print_list - Prints all list elements
+  * @node: A linked list
+  * Return: The number of nodes
+  */
 
+size_t print_list(const list_t *node)
+{
+	size_t count = 0;
+
+	while (node != NULL)
+	{
+		if (node->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", node->len, node->str);
+
+		node = node->next;
+		count++;
+	}
+
+	return (count);
+}
